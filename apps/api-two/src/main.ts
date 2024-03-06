607c58d242ab21e3c38a1b57132a77a93a3a1393
BSimpson <b.x.simpson12@gmail.com>
2024-03-06 12:27:21 +1100

    initial commit

--- apps/api-two/src/main.ts
+++ apps/api-two/src/main.ts
@@ -14,12 +14,11 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-one!' });
+  res.send({ message: 'Welcome to api-two!' });
 });
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Lib One output`, libOne());
   console.log(`Lib Two output`, libTwo());
   console.log(`ES6 Module output`, nanoid());
   console.log(`Listening at http://localhost:${port}/api`);
---
+++ libs/lib-two/README.md
@@ -0,0 +1,7 @@
+# lib-two
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build lib-two` to build the library.
